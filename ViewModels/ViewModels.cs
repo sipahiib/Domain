b4ce64d691f753e5b48d618de6b588bf43440282
İbrahim <73345010+sipahiib@users.noreply.github.com>
2021-01-05 12:13:59 +0300

    Add files via upload

---
+++ ViewModels/ViewModels.cs
@@ -0,0 +1,14 @@
+﻿using IbrahimSipahi.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IbrahimSipahi.UI.ViewModels
+{
+    public class ViewModels
+    {
+        public List<Followups> Followups { get; set; }
+        public List<Education> Educations { get; set; }
+    }
+}
